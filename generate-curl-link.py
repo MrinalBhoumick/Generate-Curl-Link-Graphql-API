128b63573b97e6b4ca36e3c043999a0ff4f158a9
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-07-12 16:55:40 +0530

    updated with authorization token

--- generate-curl-link.py
+++ generate-curl-link.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Fetch the API key and endpoint from environment variables
+# Fetch the API key, endpoint, and token from environment variables
 api_key = os.getenv('APPSYNC_API_KEY')
 graphql_endpoint = os.getenv('APPSYNC_ENDPOINT')
+auth_token = os.getenv('AUTHORIZATION_TOKEN')  # Ensure this token is set in your .env file
 
 # Ensure that the environment variables are set
-if not api_key or not graphql_endpoint:
-    raise EnvironmentError("APPSYNC_API_KEY and APPSYNC_ENDPOINT must be set as environment variables.")
+if not api_key or not graphql_endpoint or not auth_token:
+    raise EnvironmentError("APPSYNC_API_KEY, APPSYNC_ENDPOINT, and AUTHORIZATION_TOKEN must be set as environment variables.")
 
 # Define the GraphQL query
 graphql_query = """
@@ -25,13 +26,13 @@ query MyQuery {
 }
 """
 
-
 escaped_query = graphql_query.replace('\n', '').replace('"', '\\"')
 
-# Construct the curl command string
+# Construct the cURL command string with the authorization token
 curl_cmd = (
     f"curl -i -H 'Content-Type: application/json' "
-    f"-H 'x-api-key:{api_key}' "
+    f"-H 'x-api-key: {api_key}' "
+    f"-H 'Authorization: {auth_token}' "
     f"-X POST -d '{{\"query\": \"{escaped_query}\"}}' "
     f"{graphql_endpoint}"
 )
@@ -39,6 +40,6 @@ curl_cmd = (
 def generateCurlLink():
     print(curl_cmd)
 
-print("Generating curl link to AppSync endpoint")
+print("Generating cURL link to AppSync endpoint")
 generateCurlLink()
-print("Finished generating curl link to AppSync endpoint")
+print("Finished generating cURL link to AppSync endpoint")
